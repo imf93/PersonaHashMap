0e300774dd8a4a5be354084678abe14235e91def
imf93 <imerinofajardo@gmail.com>
2016-06-29 12:39:13 +0200

    Initial commit

---
+++ src/StringComparable.java
@@ -0,0 +1,31 @@
+/**
+ * Created by professor on 29/06/2016.
+ */
+public class StringComparable {
+
+    public static void main(String[] args) {
+
+        String Oscar="Oscar";
+        String Judith="Judith";
+        String marcos="Marcos";
+
+        int resultado = Judith.compareTo(Oscar);
+        int resultado2 =Judith.compareTo(marcos);
+        //System.out.println(resultado2);
+
+        Integer x=1;
+        Integer y=-2;
+
+        //System.out.println(y.compareTo(x));
+
+
+        Persona ivan=new Persona(1L,"47666936B","Ivan",22);
+        Persona marcos1=new Persona(2L,"47634763467C","Marcos",23);
+
+        System.out.println(ivan.compareTo(marcos1));
+
+
+    }
+
+
+}
